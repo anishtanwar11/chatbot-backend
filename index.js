6a1cb65bc3cd4314c534e6f0fdac6bcd0464122b
Anish <anishtanvar@gmail.com>
2023-12-08 13:11:05 +0530

    Correct Post request in index.js

--- index.js
+++ index.js
@@ -28,14 +28,23 @@ app.get("/", async (req, res) => {
 
 app.post("/chat", async (req, res) => {
   const body = await req.body;
+
+  if (!body || !body.pastMessages) {
+    // Handle the case where body or pastMessages is undefined
+    return res.status(400).json({ error: 'Invalid request body' });
+  }
+
   const input = body.input;
   const pastMessages = body.pastMessages.map((msg) => {
     if (msg.type === "human") return new HumanMessage(msg.text);
-    if (msg.type === "ai") return new AIMessage(msg.text);
+    if (msg.type === "ai"){
+      for(let i = 0; i < msg.paras.length; i++){
+        return new AIMessage(msg.paras[i]);
+      }
+    } 
   });
-
   const answer = await callChain(input, pastMessages);
-  res.json(answer)
+  res.json(answer);
 });
 
 
